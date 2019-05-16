3724c8a1d15bb9d20d97851fc59432f5ab9a44b0
sarthaksavvy <sarthak@bitfumes.com>
2019-05-17 00:45:25 +0530

    lesson 18

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,13 @@
 <template>
   <v-app>
     <v-content>
-      <v-toolbar>
+      <v-toolbar color="indigo" dark>
         <v-toolbar-side-icon></v-toolbar-side-icon>
         <v-toolbar-title>Learning Nuxt</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
           <v-btn flat>
-            <n-link to="/">Home</n-link>
-          </v-btn>
-          <v-btn flat>
-            <n-link to="/about">About</n-link>
-          </v-btn>
-          <v-btn flat>
-            <n-link to="/inspire">Inspire</n-link>
+            <n-link to="/weather-app">Weather App</n-link>
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
@@ -28,4 +22,9 @@
 export default {}
 </script>
 
-<style></style>
+<style>
+a {
+  text-decoration: none;
+  color: white;
+}
+</style>
