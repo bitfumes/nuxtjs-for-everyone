38164ef644922b498e5e174d64c7143b96d39122
sarthaksavvy <sarthak@bitfumes.com>
2019-05-24 09:26:56 +0530

    lesson 28

--- pages/weather-app.vue
+++ pages/weather-app.vue
@@ -18,26 +18,28 @@
               </p>
             </v-flex>
             <v-flex v-if="weather.weather" xs4 class="text-xs-center">
-              <h4>Temperature</h4>
-              <h1 class="display-1">{{ weather.name }}</h1>
-              <img :src="icon" alt="weather icon" />
-              <p>
-                <span class="display-1">{{ temp() }} &deg;C</span>
-                <span class="caption ml-4">{{
-                  weather.weather[0].description
-                }}</span>
-              </p>
+              <h4>Wind & Pressure:</h4>
+              <h3 class="headline">
+                Wind: {{ weather.wind.speed }} m/s ({{ weather.wind.deg }}
+                &deg;)
+              </h3>
+              <h3 class="headline mt-4">
+                Humidity: {{ weather.main.humidity }} %
+              </h3>
+              <h3 class="headline mt-4">
+                Pressure: {{ weather.main.pressure }} hPa
+              </h3>
             </v-flex>
             <v-flex v-if="weather.weather" xs4 class="text-xs-center">
-              <h4>Temperature</h4>
-              <h1 class="display-1">{{ weather.name }}</h1>
-              <img :src="icon" alt="weather icon" />
-              <p>
-                <span class="display-1">{{ temp() }} &deg;C</span>
-                <span class="caption ml-4">{{
-                  weather.weather[0].description
-                }}</span>
-              </p>
+              <h4>Other:</h4>
+              <h3 class="headline mt-4">
+                Max Temperature:
+                {{ Math.round(weather.main.temp_max - 273) }} &deg; C
+              </h3>
+              <h3 class="headline mt-4">
+                Min Temperature:
+                {{ Math.round(weather.main.temp_min - 273) }} &deg; C
+              </h3>
             </v-flex>
           </v-layout>
         </v-card-text>
